0ec1588d240dc043df04f3478876df66910ff471
Hanni Liebler <johanna.liebler@gmail.com>
2022-10-25 21:56:12 +0200

    add dateHandler to get the Trip start and end date

---
+++ src/client/js/dateHandler.js
@@ -0,0 +1,21 @@
+const getTripStart = () => {
+  const date = document.getElementById("arrival-date").value.split("-");
+  return date.join("/");
+};
+const getTripEnd = () => {
+  const date = document.getElementById("departure_date").value.split("-");
+  return date.join("/");
+};
+
+const dayCounter = (start, end) => {
+  const tripStart = Date.parse(start);
+  const tripEnd = Date.parse(end);
+
+  const countdown = tripStart - tripEnd;
+
+  const remainingDays = Math.ceil(countdown / 86400000);
+
+  return remainingDays;
+};
+
+export { getTripStart, getTripEnd, dayCounter };
