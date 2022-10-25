fb6030e2f6ae2af51b98382ed8a7ca83d39da95c
Hanni Liebler <johanna.liebler@gmail.com>
2022-10-25 21:57:15 +0200

    add the CORS allow controll

--- src/server/server.js
+++ src/server/server.js
@@ -14,6 +14,11 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static("./dist"));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
   console.log("Example app listening on port 8080!");
@@ -27,9 +32,42 @@ const trips = [];
 app.get("/", (req, res) => {
   res.status(200).send("./dist/index.html");
 });
+app.get("/jokes/random", (req, res) => {
+  request(
+    { url: "https://joke-api-strict-cors.appspot.com/jokes/random" },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: "error", message: err.message });
+      }
 
-app.post("/send", (req, res) => {
-  const trip = req.body.trip;
-  trips.push(trip);
-  res.send(trips);
+      res.json(JSON.parse(body));
+    }
+  );
 });
+
+app.post("/save", (req, res) => {
+  if (req.body !== " ") {
+    const trip = req.body.trip;
+    trips.push(trip);
+    res.send(trips);
+  } else {
+    res.status(400).json("Bad request");
+  }
+});
+
+// app.post("/forecast", async (req, res) => {
+//   if (req.body.endpoint !== " ") {
+//     const endpoint = req.body.endpoint;
+//     try {
+//       const response = await fetch(endpoint);
+//       if (response.ok) {
+//         const jsonResponse = await response.json();
+//         res.status(200).send(jsonResponse);
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   } else {
+//     res.status(400).json("Bad request");
+//   }
+// });
